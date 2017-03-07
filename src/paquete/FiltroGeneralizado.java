8701a6baf5a358117df28dd64573a119bf8e6c0f
kimco <kimco@LAPTOP-25V3PDAH>
2017-03-06 22:12:22 -0600

    Se agregaron pruebas a los demás métodos

--- src/paquete/FiltroGeneralizado.java
+++ src/paquete/FiltroGeneralizado.java
@@ -1,11 +1,97 @@
 package paquete;
 
+import org.junit.Test;
+
 public class FiltroGeneralizado {
 	public static void main( String[] args ){
 		filter();
+		FiltroGeneralizado fg = new FiltroGeneralizado();
+		fg.filterTest();
 	}
 	
 	public static void filter(){
+		
+		int L = 3, K = 3;
+		//int x = 2, y = 2;
+		
+		int[][] U = {
+				{0,0,0,0,0,0,0},
+				{0,1,1,1,1,0,0},
+				{0,1,2,2,1,1,0},
+				{0,1,2,2,2,1,0},
+				{0,0,1,2,2,1,0},
+				{0,0,1,1,1,1,0},
+				{0,0,1,1,1,1,0}
+		};
+		int[][] Ures = {
+				{0,0,0,0,0,0,0},
+				{0,1,1,1,1,0,0},
+				{0,1,2,2,1,1,0},
+				{0,1,2,2,2,1,0},
+				{0,0,1,2,2,1,0},
+				{0,0,1,1,1,1,0},
+				{0,0,1,1,1,1,0}
+		};
+		
+		int [][] gaussFilter = {
+				{4,0,0},
+				{0,0,0},
+				{0,0,-4}
+		};
+
+		System.out.println("Matriz inicial");
+		for(int i=0; i < U.length; i++){			// Imprimir la matriz inicial
+			for(int j=0; j < U.length; j++){
+				System.out.print(U[i][j] + "\t");
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+		System.out.println("Matriz sin borde");
+		for(int i=0; i < U.length; i++){			// Eliminar los bordes
+			for(int j=0; j < U.length; j++){
+				if((i == 0) | (i == U.length-1) | (j == 0) | (U[0].length-1 == 0)){
+					U[i][j] = Ures[i][j] = 0;
+				};
+			}
+		}
+
+		System.out.println("Aplicando filtro...");
+		for(int x=1; x < U.length-1; x++){			// Imprimir la matriz inicial
+			for(int y=1; y < U[0].length-1; y++){
+				//System.out.print(U[x][y] + "\t");
+				int result = 0;
+				int 
+				xkmin = x-(int)Math.floor((K-1)/2), 
+				xkmax = x+(int)Math.floor((K-1)/2), 
+				ylmin = y-(int)Math.floor((L-1)/2), 
+				ylmax = y+(int)Math.floor((L-1)/2);
+				
+				for(int i=xkmin; i < xkmax+1; i++){			// Filtro
+					for(int j=ylmin; j < ylmax+1; j++){
+						result += U[i][j] * gaussFilter[i%gaussFilter.length][j%gaussFilter.length];
+						System.out.print(U[i][j] + " ");
+					}
+					System.out.println();
+				}
+				Ures[x][y] = result;
+			}
+			System.out.println();
+		}
+		
+		System.out.println("Matriz resultado");
+		for(int i=0; i < Ures.length; i++){
+			for(int j=0; j < Ures.length; j++){
+				System.out.print(Ures[i][j] + "\t");
+			}
+			System.out.println();
+		}
+	}
+	
+	
+@Test
+public void filterTest(){
 		
 		int L = 3, K = 3;
 		//int x = 2, y = 2;
--- src/paquete/Filtros.java
+++ src/paquete/Filtros.java
@@ -1,14 +1,108 @@
 package paquete;
 
+import org.junit.Test;
 
 public class Filtros
 {
 	
 	public static void main( String[] args ){
 		filter();
+		Filtros fl = new Filtros();
+		fl.filterTest();
 	}
 	
 	public static void filter(){
+		
+		int[][] U = {
+				{0,0,0,0,0,0,0},
+				{0,1,1,1,1,0,0},
+				{0,1,2,2,1,1,0},
+				{0,1,2,2,2,1,0},
+				{0,0,1,2,2,1,0},
+				{0,0,1,1,1,1,0},
+				{0,0,1,1,1,1,0}
+		};
+		int[][] Ures = {
+				{0,0,0,0,0,0,0},
+				{0,1,1,1,1,0,0},
+				{0,1,2,2,1,1,0},
+				{0,1,2,2,2,1,0},
+				{0,0,1,2,2,1,0},
+				{0,0,1,1,1,1,0},
+				{0,0,1,1,1,1,0}
+		};
+		
+		int [][] gaussFilter = {
+				{4,0,0},
+				{0,0,0},
+				{0,0,-4}
+		};
+
+		for(int i=0; i < U.length; i++){			// Imprimir la matriz inicial
+			for(int j=0; j < U.length; j++){
+				System.out.print(U[i][j] + "\t");
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+		for(int i=0; i < U.length; i++){			// Eliminar los bordes
+			for(int j=0; j < U.length; j++){
+				if((i == 0) | (i == U.length-1) | (j == 0) | (U[0].length-1 == 0)){
+					U[i][j] = Ures[i][j] = 0;
+				};
+			}
+		}
+
+		for(int i=0; i < Ures.length; i++){			// Imprimir la matriz resultado
+			for(int j=0; j < Ures.length; j++){
+				System.out.print(Ures[i][j] + "\t");
+			}
+			System.out.println();
+		}
+
+
+		for(int i=1; i < U.length-1; i++){			// 
+			for(int j=1; j < U.length-1; j++){
+				int result = 
+						U[i-1][j-1] * gaussFilter[0][0]  +
+						U[i  ][j-1] * gaussFilter[0][1]  +
+						U[i+1][j-1] * gaussFilter[0][2]  +
+						U[i-1][j  ] * gaussFilter[1][0]  +
+						U[i  ][j  ] * gaussFilter[1][1]  +
+						U[i+1][j  ] * gaussFilter[1][2]  +
+						U[i-1][j+1] * gaussFilter[2][0]  + 
+						U[i  ][j+1] * gaussFilter[2][1]  +
+						U[i+1][j+1] * gaussFilter[2][2];
+				Ures[i][j] = result;
+				/*System.out.print  (U[i-1][j-1] + "*" + gaussFilter[0][0] + " ");
+				System.out.print  (U[i  ][j-1] + "*" + gaussFilter[0][1] + " ");
+				System.out.println(U[i+1][j-1] + "*" + gaussFilter[0][2] + " ");
+				System.out.print  (U[i-1][j  ] + "*" + gaussFilter[1][0] + " ");
+				System.out.print  (U[i  ][j  ] + "*" + gaussFilter[1][1] + " ");
+				System.out.println(U[i+1][j  ] + "*" + gaussFilter[1][2] + " ");
+				System.out.print  (U[i-1][j+1] + "*" + gaussFilter[2][0] + " ");
+				System.out.print  (U[i  ][j+1] + "*" + gaussFilter[2][1] + " ");
+				System.out.println(U[i+1][j+1] + "*" + gaussFilter[2][2] + " ");
+				
+				System.out.print("\t\t" + result);
+				System.out.println();
+				*/
+			}
+			//System.out.println();
+		}
+		System.out.println();
+		
+		for(int i=0; i < Ures.length; i++){
+			for(int j=0; j < Ures.length; j++){
+				System.out.print(Ures[i][j] + "\t");
+			}
+			System.out.println();
+		}
+	}
+	
+@Test
+public void filterTest(){
 		
 		int[][] U = {
 				{0,0,0,0,0,0,0},
