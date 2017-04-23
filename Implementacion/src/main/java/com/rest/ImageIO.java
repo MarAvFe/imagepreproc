9260ca4280c228707b537342c8b0a2ec5862a2ea
Lagartello <marcelloavila@gmail.com>
2017-04-22 18:50:50 -0600

    WebService implementado simple. Retorna un mensaje de texto

---
+++ Implementacion/src/main/java/com/rest/ImageIO.java
@@ -0,0 +1,21 @@
+package main.java.com.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("prifu")
+public class ImageIO {
+ 
+	@GET
+	@Path("/{param}")
+	public Response getMsg(@PathParam("param") String msg) {
+ 
+		String output = "Jersey say : " + msg;
+ 
+		return Response.status(200).entity(output).build();
+ 
+	}
+ 
+}
